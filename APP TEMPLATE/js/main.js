7911c5f88b072ec136bdc86cf748a04b02719ee5
Darren George Outram <doutram1027@gmail.com>
2016-11-02 14:29:17 +0000

    APP TEMPLATE AND HAULR. LOGO
    I HAVE ADDED THE APP TEMPLATE AND HAULR. LOGO

---
+++ APP TEMPLATE/js/main.js
@@ -0,0 +1,17 @@
+//var ... window.jquery: // create shortcut for jquery reference.
+
+// jquery show and hide //
+var Backbutton = $('#back')
+var GoButton = $('#home button');
+var HomeSection = $('#home')
+var Results = $('#results')
+// tell the button to do something when we click it 
+GoButton.click(function(){
+    HomeSection.hide()
+    Results.show()
+})
+
+Backbutton.click(function(){
+Results.hide()
+HomeSection.show()
+})
